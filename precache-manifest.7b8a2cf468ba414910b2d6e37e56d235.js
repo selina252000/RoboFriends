e2b29b19593d7fa5353d0c75f29e030050c9f4d2
Selina252000 <selina.darnal@netsmartz.net>
2022-09-08 12:18:57 +0530

    Updates

---
+++ precache-manifest.7b8a2cf468ba414910b2d6e37e56d235.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "94a92d3466f3a51f6c35",
+    "url": "/RoboFriends/static/css/main.093a2428.chunk.css"
+  },
+  {
+    "revision": "94a92d3466f3a51f6c35",
+    "url": "/RoboFriends/static/js/main.94a92d34.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RoboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "3a5df2eac58f9166371e",
+    "url": "/RoboFriends/static/css/2.b4fd0793.chunk.css"
+  },
+  {
+    "revision": "3a5df2eac58f9166371e",
+    "url": "/RoboFriends/static/js/2.3a5df2ea.chunk.js"
+  },
+  {
+    "revision": "4cb7496509aa3077e2b1",
+    "url": "/RoboFriends/static/js/runtime~main.4cb74965.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e71ed5389568fa5d41243334fa95b382",
+    "url": "/RoboFriends/index.html"
+  }
+];
